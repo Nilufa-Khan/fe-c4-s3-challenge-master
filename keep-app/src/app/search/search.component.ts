e5520a90cbc9b931b648dc2d463dab5ddd1b30dc
Nilufa Khan <khannilufa5@gmail.com>
2023-01-04 00:01:40 +0530

    Initial commit

---
+++ keep-app/src/app/search/search.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit ,Output,EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  searchText:string = "";
+
+   
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+   @Output()
+   searchs:EventEmitter<string> = new EventEmitter<string>();
+
+
+   se(){
+    this.searchs.emit(this.searchText)
+   }
+
+
+}
